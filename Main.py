afdf35597e93abcd153d11c42c786a83f95151c9
judahdaboss <46258035+judahdaboss@users.noreply.github.com>
2018-12-30 12:10:55 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ async def on_member_join(member):
 	embed = discord.Embed(title="👋 {} just joined {} server!".format(member.name, server.name), description="Welcome to {} {}! Enjoy your stay here!".format(server.name, member.name), color=0x00ff00)
 	embed.set_thumbnail(url=member.avatar_url)
 try:
-	await bot.send_message(discord.utils.get(server.channels, name = "join-leave"), embed=embed)
+	await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
 except discord.errors.InvalidArgument:
 	await bot.create_channel(server, "joins-and-leaves", type=discord.ChannelType.text)
 	await bot.send_message(discord.utils.get(server.channels, name="joins-and-leaves"), embed=embed)
