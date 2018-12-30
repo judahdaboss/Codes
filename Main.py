84c90327195b332a9b65158777b83f97a6455ad0
judahdaboss <46258035+judahdaboss@users.noreply.github.com>
2018-12-30 00:41:53 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,15 +15,13 @@ async def on_member_join(member):
     server = member.server
     embed = discord.Embed(title="👋 {} just joined {} server!".format(member.name, server.name), description="Welcome to {} {}! Enjoy your stay here!".format(server.name, member.name), color=0x00ff00)
     embed.set_thumbnail(url=member.avatar_url)
-    try:
-        await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
+    await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
 
 @bot.event
 async def on_member_remove(member):
     server = member.server
     embed = discord.Embed(title="👋 {} just left the server.".format(member.name), description="Goodbye! {} hope to see you again".format(member.name), color=0x00ff00)
     embed.set_thumbnail(url=member.avatar_url)
-    try:
-        await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
+    await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
         
 bot.run(os.environ['BOT_TOKEN'])
