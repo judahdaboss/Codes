23d331362da83a55127fff82d552966718d99710
judahdaboss <46258035+judahdaboss@users.noreply.github.com>
2018-12-30 01:58:22 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,14 +16,22 @@ async def on_member_join(member):
     server = member.server
     embed = discord.Embed(title="👋 {} just joined {} server!".format(member.name, server.name), description="Welcome to {} {}! Enjoy your stay here!".format(server.name, member.name), color=0x00ff00)
     embed.set_thumbnail(url=member.avatar_url)
-    await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
+    try:
+        await bot.send_message(discord.utils.get(server.channels, name = "join-leave"), embed=embed)
+    except discord.errors.InvalidArgument:
+        await bot.create_channel(server, "joins-and-leaves", type=discord.ChannelType.text)
+        await bot.send_message(discord.utils.get(server.channels, name="joins-and-leaves"), embed=embed)
 
 @bot.event
 async def on_member_remove(member):
     server = member.server
     embed = discord.Embed(title="👋 {} just left the server.".format(member.name), description="Goodbye! {} hope to see you again".format(member.name), color=0x00ff00)
     embed.set_thumbnail(url=member.avatar_url)
-    await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
+    try:
+        await bot.send_message(discord.utils.get(server.channels, name = "joins-and-leaves"), embed=embed)
+    except discord.errors.InvalidArgument:
+        await bot.create_channel(server, "joins-and-leaves", type=discord.ChannelType.text)
+        await bot.send_message(discord.utils.get(server.channels, name="joins-and-leaves"), embed=embed)
     
 def user_is_me(ctx):
 	return ctx.message.author.id == "277983178914922497"
