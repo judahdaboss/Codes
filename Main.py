0753138fcee2a4a71cb121390a59616fac71a0d3
judahdaboss <46258035+judahdaboss@users.noreply.github.com>
2018-12-30 00:39:11 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,12 @@ import os
 bot = commands.Bot(command_prefix=("!p"))
 
 @bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+
+@bot.event
 async def on_member_join(member):
     server = member.server
     embed = discord.Embed(title="👋 {} just joined {} server!".format(member.name, server.name), description="Welcome to {} {}! Enjoy your stay here!".format(server.name, member.name), color=0x00ff00)
